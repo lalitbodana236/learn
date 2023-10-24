f155f1d518049746b8dcc7170722ce93641c6893
lalitbodana236 <lalitbodana236@gmail.com>
2023-10-24 11:06:47 +0530

    find median using heap

---
+++ src/study/heap/FindMedian.java
@@ -0,0 +1,34 @@
+package study.heap;
+
+import java.util.PriorityQueue;
+
+public class FindMedian {
+	PriorityQueue<Integer> maxHeap;
+	PriorityQueue<Integer> minHeap;
+
+	public FindMedian() {
+		maxHeap = new PriorityQueue<>((a,b)->b-a);
+		minHeap = new PriorityQueue<>((a,b)->a-b);
+
+	}
+
+	public void insertNum(int num) {
+		if(maxHeap.isEmpty() || maxHeap.peek()>num) {
+			maxHeap.add(num);
+		}else {
+			minHeap.add(num);
+		}
+		if(maxHeap.size()>minHeap.size()+1) {
+			minHeap.add(maxHeap.poll());
+		}else if(maxHeap.size()<minHeap.size()) {
+			maxHeap.add(minHeap.poll());
+		}
+	}
+
+	public double findMedian() {
+		if(maxHeap.size()==minHeap.size()) {
+			return maxHeap.poll()/2.0 + minHeap.poll()/2.0;
+		}
+		return maxHeap.poll();
+	}
+}
